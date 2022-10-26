a84e30d0bc1dd3dc9a946d0680a6eead56fee08b
Maximiliano Danne <dmaxi90@gmail.com>
2022-10-25 23:43:00 -0300

    Agregar archivos de proyecto.

---
+++ Sesion1-Ej1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion1_Ej1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Ingrese su nombre: ");
+            nombre = Console.ReadLine().ToString();
+            Console.WriteLine(nombre);
+        }
+    }
+}
---
+++ Sesion1-Ej2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion1_Ej2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss"));
+        }
+    }
+}
