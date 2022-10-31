25d07240c09722c4888ec7585f1c1a9a6ec2a465
Maximiliano Danne <dmaxi90@gmail.com>
2022-10-31 20:00:07 -0300

    Se agrego actividades de Sesion3

---
+++ Sesion3-Ej1/Program.cs
@@ -0,0 +1,28 @@
+﻿Datos misDatos = new Datos("Maximiliano", 154568602, "Sarmiento 123", "maxi@maxi.com", true);
+Console.WriteLine(misDatos.NombreCompleto);
+Console.WriteLine(misDatos.Telefono);
+Console.WriteLine(misDatos.Direccion);
+Console.WriteLine(misDatos.Email);
+Console.WriteLine(misDatos.NuevoCliente);
+Console.ReadLine();
+public struct Datos
+{
+    public Datos (string nombreCompleto, int telefono, string direccion, string email, bool nuevoCliente)
+    {
+        NombreCompleto = nombreCompleto;
+        Telefono = telefono;
+        Direccion = direccion;
+        Email = email;
+        NuevoCliente = nuevoCliente;
+    }
+
+    public string NombreCompleto { get; }
+    public int Telefono { get; }
+    public string Direccion { get; }
+    public string Email { get; }
+    public bool NuevoCliente { get; }
+
+
+    public override string ToString() => $"({NombreCompleto},{Telefono},{Direccion},{Email},{NuevoCliente})";
+   
+}
